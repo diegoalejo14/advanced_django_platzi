95a39292c3fb7ab53fb69cfd856d46e9aa6d7856
Alejandro Olivera <da.olivera@mensajerosurbanos.com>
2021-11-23 14:13:15 -0500

    Add Membership Model

--- cride/circles/models/circles.py
+++ cride/circles/models/circles.py
@@ -1,40 +1,45 @@
 """Circles  model."""
 
-#Django
+# Django
 from django.db import models
 
-#Utils
+# Utils
 from cride.utils.models import CRideModel
 
 
 class Circle(CRideModel):
-  """Circle model.
-  
-    Circle is a private group. Yo Joain a circle yo need a unqiue invitation
-  """
+    """Circle model.
 
-  name=models.CharField('Circle name',max_length=140)
-  slug_name=models.SlugField(unique=True,max_length=40)
-  about=models.CharField('circle description',max_length=255)
-  picture=models.ImageField(upload_to='circles/pictures',blank=True,null=True)
+      Circle is a private group. Yo Joain a circle yo need a unqiue invitation
+    """
 
-  #Stats
-  rides_offered=models.PositiveIntegerField(default=0)
-  rides_taken=models.PositiveIntegerField(default=0)
+    name = models.CharField('Circle name', max_length=140)
+    slug_name = models.SlugField(unique=True, max_length=40)
+    about = models.CharField('circle description', max_length=255)
+    picture = models.ImageField(upload_to='circles/pictures', blank=True, null=True)
+    members = models.ManyToManyField(
+        'users.User',
+        through='circles.Membership',
+        through_fields=('circle','user')
+    )
 
-  verified=models.BooleanField('verify circle',default=False,help_text='Verified circles are also kwnown as official')
-  is_public=models.BooleanField(default=True,help_text='Public circles are listed in the main page so everyone known about their existence')
-  is_limited=models.BooleanField(default=False,help_text='Limited circles can grow up to a fixed number of members.')
-  members_limit= models.PositiveIntegerField(default=0,help_text='If circle is limited, this will be the maximum number of members in the circle')  
+    # Stats
+    rides_offered = models.PositiveIntegerField(default=0)
+    rides_taken = models.PositiveIntegerField(default=0)
 
+    verified = models.BooleanField('verify circle', default=False,
+                                   help_text='Verified circles are also kwnown as official')
+    is_public = models.BooleanField(
+        default=True, help_text='Public circles are listed in the main page so everyone known about their existence')
+    is_limited = models.BooleanField(
+        default=False, help_text='Limited circles can grow up to a fixed number of members.')
+    members_limit = models.PositiveIntegerField(
+        default=0, help_text='If circle is limited, this will be the maximum number of members in the circle')
 
-  def __str__(self) -> str:
-      """Return circle name"""
-      return self.name
+    def __str__(self) -> str:
+        """Return circle name"""
+        return self.name
 
-  
-  class Meta(CRideModel.Meta):
-    """Meta option"""
-    ordering=['-rides_taken','-rides_offered']
-
-
+    class Meta(CRideModel.Meta):
+        """Meta option"""
+        ordering = ['-rides_taken', '-rides_offered']
