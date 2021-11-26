8c3226510d120125ab55f55b7609c7286c891821
Alejandro Olivera <da.olivera@mensajerosurbanos.com>
2021-11-26 10:53:26 -0500

    Add process to update profile and get members info

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -15,15 +15,21 @@
 
 from rest_framework.routers import DefaultRouter
 
-from django.urls import include,path
+from django.urls import include, path
 
 # Views
 from .views import circles as circle_views
+from .views import memberships as memberships_views
 
 router = DefaultRouter()
 router.register('circles', circle_views.CircleViewSet, basename='circle')
+router.register(r'circles/(?P<slug_name>[a-zA-Z0-9_-]+)/members',
+                memberships_views.MembershipViewSet,
+                basename='membership'
+
+                )
 
 
-urlpatterns=[
-  path('',include(router.urls))
+urlpatterns = [
+    path('', include(router.urls))
 ]
