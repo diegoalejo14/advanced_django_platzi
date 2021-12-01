5a7515c92281f05a9aef68e9ea2ef43c80297257
Alejandro Olivera <da.olivera@mensajerosurbanos.com>
2021-12-01 04:46:44 -0500

    Add rides app

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -11,6 +11,9 @@ from cride.circles.serializers import CircleModelSerializer
 from cride.circles.models import Circle, Membership
 from cride.circles.permissions.circles import IsCircleAdmin
 
+# Filters
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CircleViewSet(mixins.CreateModelMixin,
@@ -22,8 +25,16 @@ class CircleViewSet(mixins.CreateModelMixin,
     """Circle view set."""
 
     serializer_class = CircleModelSerializer
-    #Change get or update process to id to slug_name
-    lookup_field='slug_name'
+    # Change get or update process to id to slug_name
+    lookup_field = 'slug_name'
+
+    # Filters
+    filter_backends = (SearchFilter, OrderingFilter, DjangoFilterBackend)
+    search_fields = ('slug_name', 'name')
+    ordering_fields = ('rides_offered', 'rides_taken', 'name', 'created', 'member_limit')
+    ordering = ('-members__count', '-rides_offered', '-rides_taken')
+    filter_fields=('verified','is_limited')
+
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
@@ -35,8 +46,8 @@ class CircleViewSet(mixins.CreateModelMixin,
 
     def get_permissions(self):
         """Assign permissions bases on action."""
-        permissions=[IsAuthenticated]
-        if self.action in ['update','partial_update']:
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
             permissions.append(IsCircleAdmin)
         return [p() for p in permissions]
 
---
+++ cride/rides/apps.py
@@ -0,0 +1,11 @@
+"""Circles app."""
+
+# Django
+from django.apps import AppConfig
+
+
+class RidesAppConfig(AppConfig):
+    """Circles app config."""
+
+    name = 'cride.rides'
+    verbose_name = 'Rides'
